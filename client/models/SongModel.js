b6e44100f68f6cc8c869aec045bb9114e1dd67a9
Kelly von Borstel <kellyvonborstel@gmail.com>
2016-05-01 22:56:18 -0700

    add enqueue, dequeue, and ended methods to SongModel

--- client/models/SongModel.js
+++ client/models/SongModel.js
@@ -1,9 +1,24 @@
-// SongModel.js - Defines a backbone model class for songs
+// defines a backbone model class for songs
 var SongModel = Backbone.Model.extend({
 
   play: function(){
-    // Triggering an event here will also trigger the event on the collection
+    // triggering an event here will also trigger the event on the collection
     this.trigger('play', this);
+  },
+
+  enqueue: function(){
+    // triggering an event here will also trigger the event on the collection
+    this.trigger('enqueue', this);
+  },
+
+  dequeue: function(){
+    // triggering an event here will also trigger the event on the collection
+    this.trigger('dequeue', this);
+  },
+
+  ended: function(){
+    // triggering an event here will also trigger the event on the collection
+    this.trigger('ended', this);
   }
 
 });
