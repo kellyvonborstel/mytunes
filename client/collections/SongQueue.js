444f6c8af16aab2c7858e8802eaddd5ac32aaa81
Kelly von Borstel <kellyvonborstel@gmail.com>
2016-05-01 23:24:07 -0700

    implement initialize, enqueue, and dequeue methods in SongQueue

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -1,7 +1,24 @@
-// SongQueue.js - Defines a backbone model class for the song queue
+// defines a backbone model class for the song queue
 var SongQueue = Songs.extend({
 
-  initialize: function(){
-  }
+  initialize: function() {
+    this.on('add', this.enqueue, this);
+    this.on('dequeue', this.dequeue, this);
+    this.on('ended', this.playNext, this);
+  },
 
+  enqueue: function(song) {
+    if (this.length === 1) {
+      this.playFirst();
+    }
+  },
+
+  dequeue: function(song) {
+    if (this.at(0) === song) {
+      this.playNext();
+    } else {
+      this.remove(song);
+    }
+  }
+  
 });
