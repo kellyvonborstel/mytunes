6ec2b843eb84ccf9e3d2cca2c4953af93bd45a60
Kelly von Borstel <kellyvonborstel@gmail.com>
2016-05-01 23:48:37 -0700

    add songQueueView to render method in AppView

--- client/views/AppView.js
+++ client/views/AppView.js
@@ -1,13 +1,16 @@
-// AppView.js - Defines a backbone view class for the whole music app
+// defines a backbone view class for the whole music app
 var AppView = Backbone.View.extend({
 
-  initialize: function(params){
-    this.playerView = new PlayerView({model: this.model.get('currentSong')});
-    this.libraryView = new LibraryView({collection: this.model.get('library')});
+  initialize: function(params) {
+    this.playerView = new PlayerView({
+      model: this.model.get('currentSong')
+    });
 
-    // change:currentSong - this is Backbone's way of allowing you to filter events
-    // to ONLY receive change events for the specific property, 'currentSong'
-    this.model.on('change:currentSong', function(model){
+    this.libraryView = new LibraryView({
+      collection: this.model.get('library')
+    });
+
+    this.model.on('change:currentSong', function(model) {
       this.playerView.setSong(model.get('currentSong'));
     }, this);
   },
@@ -15,7 +18,8 @@ var AppView = Backbone.View.extend({
   render: function(){
     return this.$el.html([
       this.playerView.$el,
-      this.libraryView.$el
+      this.libraryView.$el,
+      this.songQueueView.$el
     ]);
   }
 
